ba471be49fbdc269cd9f69edc1740ddb3008fce8
Wojciech Dłubacz <wdsahadar@gmail.com>
2015-12-08 22:22:29 +0100

    Async tests

--- test/test.js
+++ test/test.js
@@ -5,9 +5,25 @@ QUnit.module('tiny-deferred', {
 	}
 })
 
-test("Is Promise test", function () {
-    var value = "hello";
+test("Is Promise test", 1, function () {
     var defer = deferred();
 
     ok(isPromise(defer.promise), "Promise");
+});
+
+asyncTest("Async qunit test", function () {
+    setTimeout(function() {
+		start();
+		ok(true, "teeest");
+    }, 100);
+});
+
+asyncTest("No arguments", function () {
+    var defer = deferred();
+    var value = 'some_value';
+
+	defer.resolve(value)(function (res) {
+		start();
+		ok(res === value, "Resolve");
+	});
 });
